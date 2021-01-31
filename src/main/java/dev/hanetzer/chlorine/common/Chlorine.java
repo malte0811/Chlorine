a59edead965b08c7fdd88668b2e1b9ba324029fd
malte0811 <malte0811@web.de>
2021-01-31 18:44:14 +0100

    Mark as "not required on the server", closes #13

--- src/main/java/dev/hanetzer/chlorine/common/Chlorine.java
+++ src/main/java/dev/hanetzer/chlorine/common/Chlorine.java
@@ -2,10 +2,16 @@ package dev.hanetzer.chlorine.common;
 
 import dev.hanetzer.chlorine.common.config.Config;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,5 +27,10 @@ public class Chlorine {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.clientSpec);
         ftbChunksLoaded = ModList.get().isLoaded("ftbchunks");
         MinecraftForge.EVENT_BUS.register(this);
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> Config::register);
+        ModLoadingContext.get().registerExtensionPoint(
+                ExtensionPoint.DISPLAYTEST,
+                ()->Pair.of(()->FMLNetworkConstants.IGNORESERVERONLY, (s,b)->true)
+        );
     }
 }
